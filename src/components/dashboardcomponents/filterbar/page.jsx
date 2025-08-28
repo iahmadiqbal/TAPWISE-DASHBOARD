51b59f4578461b18fa7c574b2bc95bda8a8fefc2
Itx-Ahmed <ahmadiqbal858687@gmail.com>
2025-08-28 06:28:56 -0700

    filterpage updated

--- src/components/dashboardcomponents/filterbar/page.jsx
+++ src/components/dashboardcomponents/filterbar/page.jsx
@@ -4,7 +4,7 @@ import { FiCalendar } from "react-icons/fi";
 
 const FilterBar = () => {
   return (
-    <div className="mt-6 w-full rounded-lg   bg-white p-4 flex flex-col gap-3 sm:flex-row sm:items-center sm:gap-3 border border-solid border-[var(--border-color)]">
+    <div className="mt-6 w-full rounded-lg   bg-white p-7 flex flex-col gap-3 sm:flex-row sm:items-center sm:gap-3 border border-solid border-[var(--border-color)]">
       {/* User Select */}
       <select className="w-full sm:w-1/3 rounded-lg border border-[var(--border-color)] py-[10px] px-[12px] bg-white text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-[#FFD900]">
         <option>Select user</option>
@@ -25,7 +25,6 @@ const FilterBar = () => {
       <button className="rounded-md bg-[#FFD900] px-6 py-2 text-sm font-medium text-black hover:bg-[#ffe657] transition-colors">
         Apply
       </button>
-
       {/* Reset */}
       <button className="rounded-md border border-gray-300 px-6 py-2 text-sm font-medium text-gray-700 bg-white hover:bg-gray-100 transition-colors">
         Reset
